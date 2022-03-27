f11ef0174cccc0a62e5f9a845456dc147a01a31d
shamsu-uddin <shamsumohammaduddin@gmail.com>
2022-03-27 17:22:38 -0400

    03/27/22 5:22

---
+++ src/seleniumHomeWorkShamsu/HW_15_03152022_Dynamic_Xpath.java
@@ -0,0 +1,39 @@
+package seleniumHomeWorkShamsu;
+
+ public class HW_15_03152022_Dynamic_Xpath {
+	
+//	ebay.com ---search field------------> //input[@class='gh-tb ui-autocomplete-input']
+//	ebay.com ---search boton------------> //input[@class='btn btn-prim gh-spr']
+//	ebay.com ---select boton------------> //select[@class='gh-sb ']
+//	ebay.com ---select navigation-desktop----> //div[@class='navigation-desktop']
+//	ebay.com ---select ebay Help------------>//a[@class='lia-link-navigation custom-ebay-help-link']
+//
+//	ebay.com ---select boton------------> //select[@name='_sacat']
+//	ebay.com ---search field------------> //input[@name='_nkw']
+//	ebay.com ---search field------------> //input[@name= '_sop']
+//	https://www.bestbuy.com --search field-> //input[@name='st']
+//	https://www.bestbuy.com --search email signup-> //input[@name='footer-email-signup']
+//
+//	ebay.com ---search boton------------> //input[@id='gh-btn']
+//	ebay.com ---select boton------------> //select[@id='gh-cat']
+//	ebay.com ---select ebay logo------------> //img[@id='gh-logo']
+//	ebay.com ---select Home & garden------------> //a[@id='nav_368255']
+//	ebay.com ---select Sporting Goods------------> //a[@id='nav_368256']
+//	ebay.com ---select fashion ------------> //a[@id='nav_368254']
+//
+//	ebay.com ---select featured------------> //a[@class='navigation-desktop-menu-link' and @id='nav_368252']
+//	https://www.bestbuy.com --search email signup-> //input[@id='footer-email-signup' and @class='tb-input email-signup-input']
+//
+//
+//	ebay.com ---select deals button------------> //button[text()='Expand Featured']
+//	ebay.com ---select Advanced ------------> //a[text()='Advanced']
+//	ebay.com ---select Help & Contact---------->//a[text()= 'Help & Contact']
+//	ebay.com ---select About ebay----------> //a[text()='About eBay']
+//	ebay.com ---select ebay Help------------> //a[text()='eBay Help']
+//
+//	https://www.bestbuy.com --search Sign in or Create Account-> //a[contains(text() ,'Si')]
+//	https://www.bestbuy.com --search Signup button------------> //button[contains(text(),'Sig')]
+//	https://www.bestbuy.com --search Get the latest deals and more.--->//h3[contains(text(),'G')]
+//	https://www.bestbuy.com --search check order status------------> //a[contains(text(),'Che')]
+//	https://www.bestbuy.com --search developers------------------->  //a[contains(text(),'Dev')]
+}
